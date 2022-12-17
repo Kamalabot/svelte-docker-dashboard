75179baca4a2d81355e32a159da7eeda74f0f2a9
solverbot <kamaljp@gmail.com>
2022-12-17 06:14:42 +0530

    rewired the homepage

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -2,19 +2,29 @@
 import parsers from "$lib/parsers"
 
 export const load = async ({fetch})=>{
-    
     const fetchData = async() =>{
-        const res = await fetch('https://api.covidtracking.com/v1/us/daily.json')
-        const dataRecd = await res.json()
-        const parsedData = parsers.historicUS(dataRecd)
-        return parsedData
-    }
-    
-    return {
-        chartData: fetchData(),
-        color:'green',
-        title:'Covid Status in US',
-        xVar:'x',
-        yVar:'y'
+    const url = 'http://localhost:5173/dbtable';
+    const options = {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json;charset=UTF-8'
+      },
+      body: JSON.stringify({
+        tableName: "factCovid"
+      })
     };
+    const res = await fetch('https://api.covidtracking.com/v1/us/daily.json')
+    const dataRecd = await res.json()
+    const parsedData = parsers.historicUS(dataRecd)
+    return parsedData
+  }
+    
+  return {
+      chartData: fetchData(),
+      color:'green',
+      title:'Covid Status in US',
+      xVar:'x',
+      yVar:'y'
+  };
 }; 
---
+++ src/routes/tablelist/+server.js
@@ -0,0 +1,12 @@
+export const GET = async({request, locals})=>{
+    console.log('Requesting all available tables from database');
+     
+    const {sql} = locals;
+     
+    const tableNames = await sql`SELECT table_name FROM information_schema.tables WHERE table_schema='public'`
+    const tableData = {tableNames} 
+    console.log(tableData,'API reply')
+    return new Response(JSON.stringify(tableData),{status:200})
+}
+
+ 
