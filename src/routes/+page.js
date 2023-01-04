c3e6850b0e924e0b63fc18bfcc89c893e473bffc
kamalabot <kamaljp@gmail.com>
2023-01-04 10:51:30 +0000

    checked json import from github as api is working, and providing console in local environment

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,6 +1,5 @@
 /** @type {import('./$types').PageLoad} */
 import parsers from "$lib/parsers"
-
 export const load = async ({fetch})=>{
     const fetchData = async() =>{
     const url = 'http://localhost:5173/dbtable';
@@ -17,12 +16,19 @@ export const load = async ({fetch})=>{
     const res = await fetch(url,options)
     const dataRecd = await res.json()
     const parsedData = parsers.historicUS(dataRecd.yourTable)
+    console.log(bseScripts,'json imported')
     return parsedData
   }
-    
+    const fetchGap = async()=>{
+	const gapRes = await fetch('https://kamalabot.github.io/M3nD3/ObservableData/gapminder.json')
+	const gapminder = await gapRes.json() 
+	console.log(gapminder)
+	return gapminder
+    }
   //console.log(fetchData())
   return {
       chartData: fetchData(),
+      gapData: fetchGap(),
       color:'green',
       title:'Covid Status in US',
       xVar:'x',
