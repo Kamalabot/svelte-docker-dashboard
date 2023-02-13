20bb28eb54d07cd2e74101432e6b8b8ed60d3955
solverbot <kamaljp@gmail.com>
2023-02-13 13:32:09 +0530

    updated hooks

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,18 +1,19 @@
 import postgres from 'postgres';
-
+require('dotenv').config()
 import { sequence } from '@sveltejs/kit/hooks'
 
 export const handle = async ({ event, resolve }) => {
 	//Connecting to the database in local environment
-  const test = postgres(`postgres://postgres:wrangler@pipeline-tank.coc5gkht2i7a.us-east-1.rds.amazonaws.com:5432/test`,{
+  console.log(postgres)
+  const test = postgres(`postgres://postgres:thevwn@pipeline-tank.coc5gkht2i7a.us-east-1.rds.amazonaws.com:5432/test`,{
     idle_timeout: 10,
     max_lifetime: 60 * 10});
 	
-  const users = postgres(`postgres://postgres:wrangler@pipeline-tank.coc5gkht2i7a.us-east-1.rds.amazonaws.com:5432/users`,{
+  const users = postgres(`postgres://postgres:thevwn@pipeline-tank.coc5gkht2i7a.us-east-1.rds.amazonaws.com:5432/users`,{
     idle_timeout: 10,
     max_lifetime: 60 * 10});
 	
-  const company_prog= postgres(`postgres://postgres:wrangler@pipeline-tank.coc5gkht2i7a.us-east-1.rds.amazonaws.com:5432/company_progress`,{
+  const company_prog= postgres(`postgres://postgres:thevwn@pipeline-tank.coc5gkht2i7a.us-east-1.rds.amazonaws.com:5432/company_progress`,{
     idle_timeout: 10,
     max_lifetime: 60 * 10});
 
