c4c172264632aadc8eac096ca236f9cd0425d9ed
solverbot <kamaljp@gmail.com>
2023-02-13 15:47:12 +0530

    recovered from the failure

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,19 +1,16 @@
 import postgres from 'postgres';
-require('dotenv').config()
-import { sequence } from '@sveltejs/kit/hooks'
 
 export const handle = async ({ event, resolve }) => {
 	//Connecting to the database in local environment
-  console.log(postgres)
-  const test = postgres(`postgres://postgres:thevwn@pipeline-tank.coc5gkht2i7a.us-east-1.rds.amazonaws.com:5432/test`,{
+  const test = postgres(`postgres://postgres:1234@127.0.0.1:5432/test`,{
     idle_timeout: 10,
     max_lifetime: 60 * 10});
 	
-  const users = postgres(`postgres://postgres:thevwn@pipeline-tank.coc5gkht2i7a.us-east-1.rds.amazonaws.com:5432/users`,{
+  const users = postgres(`postgres://postgres:1234@127.0.0.1:5432/users`,{
     idle_timeout: 10,
     max_lifetime: 60 * 10});
 	
-  const company_prog= postgres(`postgres://postgres:thevwn@pipeline-tank.coc5gkht2i7a.us-east-1.rds.amazonaws.com:5432/company_progress`,{
+  const company_prog= postgres(`postgres://postgres:1234@127.0.0.1:5432/company_progress`,{
     idle_timeout: 10,
     max_lifetime: 60 * 10});
 
